4baea8cdbd3636b340c8e6adaf5fdd0a5723d92d
aleitner <leitnersalex@gmail.com>
2016-11-02 13:42:07 -0400

    Added entry point

---
+++ index.js
@@ -0,0 +1,17 @@
+
+/**
+ * @module storjshare-daemon
+ * @license (AGPL-3.0)
+ */
+
+'use strict';
+
+
+/** {@link ConfigManager} */
+exports.ConfigManager = require('./lib/config');
+
+/** {@link Farmer} */
+exports.Farmer = require('./lib/farmer');
+
+/** {@link Utils} */
+exports.Utils = require('./lib/utils');
