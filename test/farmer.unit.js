14bd542caf63572a6d1513a0de255c6efb4c9896
aleitner <leitnersalex@gmail.com>
2016-11-08 17:04:19 -0500

    Adding farmer tests

--- test/farmer.unit.js
+++ test/farmer.unit.js
@@ -1,3 +1,35 @@
 'use strict';
 
 var Farmer = require('../lib/farmer');
+var ConfigManager = require('../lib/config');
+var expect = require('chai').expect;
+var proxyquire = require('proxyquire');
+var sinon = require('sinon');
+var fs = require('fs');
+var path = require('path');
+var DIR = path.basename(__dirname);
+var testJsonPath = path.join(DIR, '/_fixtures/test.json');
+var testJson = fs.readFileSync(testJsonPath);
+
+describe('Farmer', function() {
+
+  describe('@constructor', function() {
+
+    it('should work without the new keyword', function() {
+      var configManager = new ConfigManager(
+        'test',
+        JSON.parse(testJson.toString())
+      );
+      configManager.updateConfig()
+      expect(Farmer(configManager)).to.be.instanceOf(Farmer);
+    });
+
+    it('should throw an error if no ConfigManager was passed in', function() {
+      expect(function() {
+        Farmer();
+      }).to.throw('An instance of ConfigManager must be passed in');
+    });
+
+  });
+
+});
