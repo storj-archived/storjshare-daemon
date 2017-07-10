9b8f211d566bbc6f8a9958610f11774d0e91ea2e
Steve Ashman <ssa3512@gmail.com>
2017-07-10 09:04:13 -0400

    check if valid sub command by reading directory contents

--- bin/storjshare.js
+++ bin/storjshare.js
@@ -5,6 +5,18 @@
 const storjshare = require('commander');
 const {version} = require('../package');
 const {software: core, protocol} = require('storj-lib').version;
+const fs = require('fs');
+
+function checkIfValidSubcommand() {
+  if (process.argv.length > 2) {
+    let files = fs.readdirSync(__dirname) || [];
+    return files.some((file) => {
+      return file.replace('storjshare-','')
+        .replace('.js','') === process.argv[2];
+    });
+  }
+  return false;
+}
 
 storjshare
   .version(`daemon: ${version}, core: ${core}, protocol: ${protocol}`)
@@ -20,3 +32,7 @@ storjshare
   .command('killall', 'kills all shares and stops the daemon')
   .command('daemon', 'starts the daemon')
   .parse(process.argv);
+
+if (!checkIfValidSubcommand()) { 
+  storjshare.help(); 
+} 
